96dfe75a41e1af487ea497b0a71d34cbe4ef3656
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-08-04 14:55:09 +0100

    Fix incorrect class names in files

--- src/Assets/Scripts/Board.cs
+++ src/Assets/Scripts/Board.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid : MonoBehaviour
+public class Board : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

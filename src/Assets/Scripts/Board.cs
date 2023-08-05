c6fb66d09f5a7294450e462f18c754861ab48489
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-08-05 16:01:19 +0100

    Add list property and initialise value

--- src/Assets/Scripts/Board.cs
+++ src/Assets/Scripts/Board.cs
@@ -9,9 +9,13 @@ public class Board : MonoBehaviour
     // class.
     public GameObject cellPrefab;
 
+    private List<GameObject> cells;
+
     // Start is called before the first frame update
     void Start()
     {
+        this.cells = new List<GameObject>();
+
         int rows = 9;
         int columns = 9;
 

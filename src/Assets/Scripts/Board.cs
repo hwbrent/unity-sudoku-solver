162c1d57c252ade7fcd61fe794747b3b171fa62b
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-08-05 15:42:39 +0100

    Attach Cell to Board via Unity editor

--- src/Assets/Scripts/Board.cs
+++ src/Assets/Scripts/Board.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    // Set in the Unity editor.
+    // We do this so that we can dynamically create instances of the Cell
+    // class.
+    public GameObject cell;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(this.cell);
     }
 
     // Update is called once per frame
